8b63b8bd0cb66c432a6aee442ef53952ec04f1e7
baggatbt <baggatbt@gmail.com>
2019-03-20 10:31:38 -0400

    More functionality
    Added ability to add as well as remove created list items

--- scripts.js
+++ scripts.js
@@ -1,11 +1,29 @@
 let userInput = document.getElementById('userInput');
 let button = document.getElementById('enter');
 let ul = document.querySelector("ul");
+let li = document.querySelector("li");
 
+//Creates new list item
 button.addEventListener("click", function() {
 	let li = document.createElement("li");
 	li.appendChild(document.createTextNode(userInput.value));
 	ul.appendChild(li);
-	newListItem.value ='';
+	userInput.value ='';
 })
 
+//Removes a list item on click
+ul.addEventListener("click",function() {
+	let itemToBeRemoved = (event.target);
+	if (itemToBeRemoved != ul){
+
+		itemToBeRemoved.remove();
+	}
+})
+
+//Allows user to press enter to add a list item as well as clicking the button
+userInput.addEventListener("keyup", function(event) {
+  if (event.keyCode === 13) {
+   event.preventDefault();
+   document.getElementById("enter").click();
+  }
+});
